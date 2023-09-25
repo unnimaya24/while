e08ba045f7ff2fc369891801adcc55dfc03f79f5
unnimaya24 <sruthynair2425@gmail.com>
2023-09-25 07:55:17 +0530

    while

---
+++ pyt.py/string.py
@@ -0,0 +1,3 @@
+x=int(input("enter the string") )
+for i in range(len(x)):
+  print(x[i]) 
---
+++ while3.py
@@ -0,0 +1,16 @@
+ 
+num = int(input(" Please Enter the Maximum number : "))
+ 
+number = 1
+ 
+while number <= num:
+    if(number % 2 == 0):
+        print("{0}".format(number))
+    number = number + 1
+    
+
+x = int(input(" Please Enter any Maximum Number : "))
+number=1
+ 
+for x in range(1, x + 1, 2):
+    print("{0}".format(x))
